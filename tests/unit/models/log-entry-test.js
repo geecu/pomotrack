3684187ab7214fafec3b7b2c6b73402e3f2b16fa
Gunther Konig <gunther.s.konig@gmail.com>
2014-12-09 10:03:11 +0200

    add test for createLogEntry, doesn't work, this.store seems to be different to the App's store

--- tests/unit/models/log-entry-test.js
+++ tests/unit/models/log-entry-test.js
@@ -2,14 +2,25 @@ import {
   moduleForModel,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('log-entry', 'LogEntry', {
   // Specify the other units that are required for this test.
-  needs: ['model:client', 'model:project', 'model:task']
+  needs: ['store:main', 'model:client', 'model:project', 'model:task']
 });
 
 test('it exists', function() {
   var model = this.subject();
   // var store = this.store();
   ok(!!model);
+});
+
+test('it adds log entry', function() {
+  var store = this.store(),
+    task;
+  Ember.run(function(){
+    task = store.createRecord('task', {});
+    store.createLogEntry(task);
+  });
+  ok(!!task);
 });
